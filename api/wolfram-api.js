4b3407eec5036637a7aee40dce6437b0b869f4d3
Lee Brown <hello@leebrown.me>
2019-03-17 11:55:04 +0000

    Fixed incorrect wolframalpha API key processing

--- api/wolfram-api.js
+++ api/wolfram-api.js
@@ -4,12 +4,13 @@ const axios = require('axios');
 module.exports = {
     getEmbed: function(input){ 
         var authwolframalpha = process.env.WOLF_AUTH;
-        axios.get('http://api.wolframalpha.com/v2/query?input=' + input + "&appid=" + authwolframalpha.token, {},{
+        axios.get('http://api.wolframalpha.com/v2/query?input=' + input + "&appid=" + authwolframalpha, {},{
             })
             .then(response =>{ 
                 var parseString = require('xml2js').parseString;
                 var xml = response.data.toString();
                 parseString(xml, function (err, result) {
+                    //console.log(result.queryresult.error['0'].msg['0']); //See error msg for debug
                     var lines = "";
                     for(i = 0; i < result.queryresult.pod[1].subpod[0].plaintext.length; i++){
                         lines = lines + result.queryresult.pod[1].subpod[0].plaintext[i];
